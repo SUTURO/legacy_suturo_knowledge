e269c08c5f5add68ea2597ca7a98f8917b418558
Moritz Horstmann <mhorst@tzi.de>
2014-01-24 16:05:21 +0100

    KR-32 #time 2h Minor fixes, formatting.

--- suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/GetClustersService.java
+++ suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/GetClustersService.java
@@ -15,74 +15,73 @@ import ros.pkg.suturo_perception_msgs.srv.GetClusters;
  * 
  */
 public class GetClustersService {
-    private final NodeHandle handle;
+	private final NodeHandle handle;
 
-    private final String serviceName;
+	private final String serviceName;
 
-    private static final String DEFAULT_SERVICE_NAME = "/suturo/GetClusters";
+	private static final String DEFAULT_SERVICE_NAME = "/suturo/GetClusters";
 
-    enum Operation {
-	GET("get");
+	enum Operation {
+		GET("get");
 
-	private String op;
+		private String op;
 
-	Operation(String op) {
-	    this.op = op;
+		Operation(String op) {
+			this.op = op;
+		}
+
+		String getOp() {
+			return this.op;
+		}
 	}
 
-	String getOp() {
-	    return this.op;
+	/**
+	 * Access to GetClusters using given handle
+	 * 
+	 * @param handle
+	 */
+	public GetClustersService(NodeHandle handle) {
+		this(handle, DEFAULT_SERVICE_NAME);
 	}
-    }
 
-    /**
-     * Access to GetClusters using given handle
-     * 
-     * @param handle
-     */
-    public GetClustersService(NodeHandle handle) {
-	this(handle, DEFAULT_SERVICE_NAME);
-    }
+	/**
+	 * Access to GetClusters using given handle and serviceName
+	 * 
+	 * @param handle
+	 * @param serviceName
+	 */
+	public GetClustersService(NodeHandle handle, String serviceName) {
+		this.handle = handle;
+		this.serviceName = serviceName;
+	}
 
-    /**
-     * Access to GetClusters using given handle and serviceName
-     * 
-     * @param handle
-     * @param serviceName
-     */
-    public GetClustersService(NodeHandle handle, String serviceName) {
-	this.handle = handle;
-	this.serviceName = serviceName;
-    }
+	/**
+	 * Simply retrieves perceived objects from service
+	 * 
+	 * @return list of perceived objects
+	 * @throws RosException
+	 */
+	public List<PerceivedObject> getClusters() throws RosException {
+		return callService(Operation.GET);
+	}
 
-    /**
-     * Simply retrieves perceived objects from service
-     * 
-     * @return list of perceived objects
-     * @throws RosException
-     */
-    public List<PerceivedObject> getClusters() throws RosException {
-	return callService(Operation.GET);
-    }
-
-    /**
-     * Calls service with given operation and returns a list of perceived
-     * objects
-     * 
-     * @param op
-     *            Operation
-     * 
-     * @return list of perceived objects
-     * @throws RosException
-     */
-    private List<PerceivedObject> callService(Operation op) throws RosException {
-	List<PerceivedObject> objects = null;
-	GetClusters.Request req = new GetClusters.Request();
-	req.s = op.getOp();
-	ServiceClient<GetClusters.Request, GetClusters.Response, GetClusters> cl = handle
-		.serviceClient(serviceName, new GetClusters());
-	objects = cl.call(req).perceivedObjs;
-	cl.shutdown();
-	return objects;
-    }
+	/**
+	 * Calls service with given operation and returns a list of perceived
+	 * objects
+	 * 
+	 * @param op
+	 *            Operation
+	 * 
+	 * @return list of perceived objects
+	 * @throws RosException
+	 */
+	private List<PerceivedObject> callService(Operation op) throws RosException {
+		GetClusters.Request req = new GetClusters.Request();
+		req.s = op.getOp();
+		ServiceClient<GetClusters.Request, GetClusters.Response, GetClusters> cl = handle
+				.serviceClient(serviceName, new GetClusters());
+		List<PerceivedObject> objects = cl.call(req).perceivedObjs;
+		cl.shutdown();
+		return objects;
+	}
 }
--- suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/PrologBridge.java
+++ suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/PrologBridge.java
@@ -22,92 +22,92 @@ import tfjava.TFListener;
  */
 public class PrologBridge {
 
-    private static final String NODE_NAME = "suturo_knowledge_javaclient";
-    private static TFListener tf = TFListener.getInstance();
+	private static final String NODE_NAME = "suturo_knowledge_javaclient";
+	private static final int DEFAULT_TIMEOUT_MS = 10000;
+	private static TFListener tf = TFListener.getInstance();
 
-    private static Ros ros;
-    private static NodeHandle handle;
-    private static GetClustersService cluster;
+	private static Ros ros;
+	private static NodeHandle handle;
+	private static GetClustersService cluster;
 
-    private final Map<Long, Stamped<Point3d>> mapCoords = new HashMap<Long, Stamped<Point3d>>();
-    private final Map<Long, Stamped<Point3d>> odomCoords = new HashMap<Long, Stamped<Point3d>>();
+	private final Map<Long, Stamped<Point3d>> mapCoords = new HashMap<Long, Stamped<Point3d>>();
+	private final Map<Long, Stamped<Point3d>> odomCoords = new HashMap<Long, Stamped<Point3d>>();
 
-    private static void checkInitialized() {
-	ros = Ros.getInstance();
-	if (!ros.isInitialized()) {
-	    ros.init(NODE_NAME);
+	private static void checkInitialized() {
+		ros = Ros.getInstance();
+		if (!ros.isInitialized()) {
+			ros.init(NODE_NAME);
+		}
+		handle = ros.createNodeHandle();
+		if (!handle.checkMaster()) {
+			ros.logError("PrologBridge: Ros master not available");
+			throw new IllegalStateException("Ros master not available");
+		}
+
 	}
-	handle = ros.createNodeHandle();
-	if (!handle.checkMaster()) {
-	    ros.logError("PrologBridge: Ros master not available");
-	    throw new IllegalStateException("Ros master not available");
+
+	/**
+	 * Calls perception service and retrieves list of perceived objects
+	 * 
+	 * @return true if any objects were found
+	 * @throws RosException
+	 */
+	public PerceivedObject[] updatePerception() throws RosException {
+		checkInitialized();
+		if (cluster == null) {
+			cluster = new GetClustersService(handle);
+		}
+		handle.setMasterRetryTimeout(DEFAULT_TIMEOUT_MS);
+		mapCoords.clear();
+		odomCoords.clear();
+		PerceivedObject[] pos = cluster.getClusters().toArray(
+				new PerceivedObject[0]);
+		for (PerceivedObject po : pos) {
+			Stamped<Point3d> poPoint = getStamped3DPoint(po);
+			addTransformPoint("/map", poPoint, mapCoords, po.c_id);
+			addTransformPoint("/odom_combined", poPoint, odomCoords, po.c_id);
+		}
+		return pos;
 	}
 
-    }
-
-    /**
-     * Calls perception service and retrieves list of perceived objects
-     * 
-     * @return true if any objects were found
-     * @throws RosException
-     */
-    public PerceivedObject[] updatePerception() throws RosException {
-	checkInitialized();
-	if (cluster == null) {
-	    cluster = new GetClustersService(handle);
+	private static Stamped<Point3d> getStamped3DPoint(PerceivedObject po) {
+		Point cent = po.c_centroid;
+		Point3d point3d = new Point3d(cent.x, cent.y, cent.z);
+		return new Stamped<Point3d>(point3d, po.frame_id, Time.now());
 	}
-	mapCoords.clear();
-	odomCoords.clear();
-	PerceivedObject[] pos = cluster.getClusters().toArray(
-		new PerceivedObject[0]);
-	for (PerceivedObject po : pos) {
-	    Stamped<Point3d> poPoint = getStamped3DPoint(po);
-	    addTransformPoint("/map", poPoint, mapCoords, po.c_id);
-	    addTransformPoint("/odom_combined", poPoint, odomCoords, po.c_id);
+
+	private static void addTransformPoint(String target, Stamped<Point3d> in,
+			Map<Long, Stamped<Point3d>> map, long cID) {
+		try {
+			Stamped<Point3d> out = new Stamped<Point3d>();
+			tf.transformPoint(target, in, out);
+			map.put(Long.valueOf(cID), out);
+		} catch (RuntimeException e) {
+			throw e;
+		} catch (Exception e) {
+			ros.logError("TF failed: " + e);
+		}
 	}
-	return pos;
-    }
 
-    private static Stamped<Point3d> getStamped3DPoint(PerceivedObject po) {
-	Point cent = po.c_centroid;
-	Point3d point3d = new Point3d(cent.x, cent.y, cent.z);
-	return new Stamped<Point3d>(point3d, po.frame_id, Time.now());
-    }
-
-    private static void addTransformPoint(String target, Stamped<Point3d> in,
-	    Map<Long, Stamped<Point3d>> map, long cID) {
-	if (!handle.getAdvertisedTopics().contains(target)) {
-	    ros.logError("Topic " + target + " not advertised!");
-	    return;
+	/**
+	 * Retrieve transformed coordinates in map frame context.
+	 * 
+	 * @param c_id
+	 *            ID of corresponding PerceivedObject
+	 * @return Point3d object
+	 */
+	public Stamped<Point3d> getMapCoords(long c_id) {
+		return this.mapCoords.get(Long.valueOf(c_id));
 	}
-	try {
-	    Stamped<Point3d> out = new Stamped<Point3d>();
-	    tf.transformPoint(target, in, out);
-	    map.put(Long.valueOf(cID), out);
-	} catch (Exception e) {
-	    ros.logError("TF failed: " + e);
+
+	/**
+	 * Retrieve transformed coordinates in odom_combined frame context.
+	 * 
+	 * @param c_id
+	 *            ID of corresponding PerceivedObject
+	 * @return Point3d object
+	 */
+	public Stamped<Point3d> getOdomCoords(long c_id) {
+		return this.odomCoords.get(Long.valueOf(c_id));
 	}
-    }
-
-    /**
-     * Retrieve transformed coordinates in map frame context.
-     * 
-     * @param c_id
-     *            ID of corresponding PerceivedObject
-     * @return Point3d object
-     */
-    public Stamped<Point3d> getMapCoords(long c_id) {
-	return this.mapCoords.get(Long.valueOf(c_id));
-    }
-
-    /**
-     * Retrieve transformed coordinates in odom_combined frame context.
-     * 
-     * @param c_id
-     *            ID of corresponding PerceivedObject
-     * @return Point3d object
-     */
-    public Stamped<Point3d> getOdomCoords(long c_id) {
-	return this.odomCoords.get(Long.valueOf(c_id));
-    }
 }
