cd3b5b23f096a292ed50baaf631df312afabba97
Moritz Horstmann <mhorst@tzi.de>
2014-01-27 00:42:03 +0100

    KR-30 #time 30m Separation between projectroom and lab owl file for both rosnode and local prolog console.

---
+++ suturo_knowledge_foodreasoner/scripts/reasoner_local_lab.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rosrun rosprolog rosprolog suturo_knowledge_foodreasoner "" lab_hierarchy.owl
---
+++ suturo_knowledge_foodreasoner/scripts/reasoner_local_projectroom.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rosrun rosprolog rosprolog suturo_knowledge_foodreasoner "" projectroom_hierarchy.owl
