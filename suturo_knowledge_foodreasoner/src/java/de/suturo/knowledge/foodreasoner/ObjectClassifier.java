9c5dae09881c9216d32234cdb8993fe206f39215
ofenrohr <ofenrohr@tzi.de>
2014-02-13 21:55:30 +0100

    KR-64 #time 4h mit owl und prolog geprügelt

--- suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/ObjectClassifier.java
+++
@@ -1,8 +0,0 @@
-package de.suturo.knowledge.foodreasoner;
-
-import ros.pkg.geometry_msgs.msg.Point;
-import ros.pkg.suturo_perception_msgs.msg.PerceivedObject;
-
-interface ObjectClassifier {
-  public String classifyPerceivedObject(PerceivedObject obj);
-}
--- suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/ProbabilityClassifier.java
+++ suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/ProbabilityClassifier.java
@@ -1,10 +1,34 @@
 package de.suturo.knowledge.foodreasoner;
 
-import ros.pkg.geometry_msgs.msg.Point;
-import ros.pkg.suturo_perception_msgs.msg.PerceivedObject;
+import java.util.ArrayList;
 
-class ProbabilityClassifier implements ObjectClassifier {
-  public String classifyPerceivedObject(PerceivedObject obj) {
-    return "";
+class ProbabilityClassifier {
+
+  private static ArrayList<ArrayList<String>> objects = new ArrayList<ArrayList<String>>();
+  
+  public ProbabilityClassifier() {
+    
+  }
+  
+  public String classifyPerceivedObject(int avgHue, double volume) {
+    String ret = "";
+    for (ArrayList<String> entry : objects) {
+      for (String x : entry) {
+        ret += x + ", ";
+      }
+      ret += "; ";
+    }
+    return ret;
+  }
+  
+  public int appendToObjectsList(String instance, String hueMean, String hueSD, String volMean, String volSD) {
+    ArrayList<String> entry = new ArrayList<String>();
+    entry.add(instance);
+    entry.add(hueMean);
+    entry.add(hueSD);
+    entry.add(volMean);
+    entry.add(volSD);
+    objects.add(entry);
+    return objects.size();
   }
 }
