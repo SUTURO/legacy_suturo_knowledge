c7bd77433e7e50d2bdf1356d10582dca094b8ef2
ofenrohr <ofenrohr@tzi.de>
2014-02-14 14:24:58 +0100

    KR-64 #time 3h 30m classification basically working, needs better values

--- suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/PerceptionProbabilities.java
+++ suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/PerceptionProbabilities.java
@@ -1,5 +1,32 @@
 package de.suturo.knowledge.foodreasoner;
 
-class PerceptionProbabilities {
+/**
+ * Container class for the probabilities of some attributes of perceived objects
+ */
+class PerceptionProbabilities implements Comparable<PerceptionProbabilities> {
+  public final String instance; /// this is the string that prolog uses to uniquely identify an object
+  public final double hueMean;
+  public final double hueSD;
+  public final double volMean;
+  public final double volSD;
+  public Double probability; // the resulting probability
   
+  public PerceptionProbabilities(String instance, double hueMean, double hueSD,
+                                 double volMean, double volSD) {
+    this.instance = instance;
+    this.hueMean = hueMean;
+    this.hueSD = hueSD;
+    this.volMean = volMean;
+    this.volSD = volSD;
+  }
+  
+  @Override
+  public int compareTo(PerceptionProbabilities  o) {
+    return probability.compareTo(o.probability);
+  }
+  
+  @Override
+  public String toString() {
+    return instance + ": " + probability;
+  }
 }
--- suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/ProbabilityClassifier.java
+++ suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/ProbabilityClassifier.java
@@ -1,34 +1,81 @@
 package de.suturo.knowledge.foodreasoner;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 class ProbabilityClassifier {
 
-  private static ArrayList<ArrayList<String>> objects = new ArrayList<ArrayList<String>>();
+  private static ArrayList<PerceptionProbabilities> objects = new ArrayList<PerceptionProbabilities>();
   
   public ProbabilityClassifier() {
     
   }
   
+  /**
+   * Classifies an object given some values that were calculated by the 
+   * perception
+   * @param int average hue value of the object
+   * @param int volume of the object
+   * @return instance string that's used by prolog
+   */
   public String classifyPerceivedObject(int avgHue, double volume) {
-    String ret = "";
-    for (ArrayList<String> entry : objects) {
-      for (String x : entry) {
-        ret += x + ", ";
-      }
-      ret += "; ";
+    if (objects == null)
+      return "";
+    if (objects.size() < 1)
+      return "";
+    
+    for (int i = 0; i < objects.size(); i++) {
+      objects.get(i).probability = 
+          normalDistribution(avgHue, objects.get(i).hueMean, objects.get(i).hueSD);
     }
-    return ret;
+    
+    Collections.sort(objects);
+    
+    return objects.get(objects.size()-1).toString();
   }
   
-  public int appendToObjectsList(String instance, String hueMean, String hueSD, String volMean, String volSD) {
-    ArrayList<String> entry = new ArrayList<String>();
-    entry.add(instance);
-    entry.add(hueMean);
-    entry.add(hueSD);
-    entry.add(volMean);
-    entry.add(volSD);
+  /**
+   * Calculates the probability density of a value given mean and standard
+   * deviation
+   * @param double value to calculate probability of
+   * @param double mean
+   * @param double standard deviation
+   * @return the probability density
+   */
+  private static double normalDistribution(double x, double mean, double sd) {
+    return (1 / (sd * Math.sqrt(2 * Math.PI))) * 
+            Math.exp( - ((x-mean)*(x-mean) / (2*sd*sd)) );
+  }
+  
+  /**
+   * This will be called by prolog on startup to fill the list of objects that
+   * we need to classify
+   * @param String instance name in prolog
+   * @param String hue mean value
+   * @param String hue standard deviation
+   * @param String volume mean
+   * @param String volume standard deviation
+   */
+  public String appendToObjectsList(String instance, String hueMean, String hueSD, 
+                                 String volMean, String volSD) {
+    double hueMeanD;
+    double hueSDD;
+    double volMeanD;
+    double volSDD;
+    try {
+      hueMeanD = Double.parseDouble(hueMean);
+      hueSDD = Double.parseDouble(hueSD);
+      volMeanD = Double.parseDouble(volMean);
+      volSDD = Double.parseDouble(volSD);
+    } catch (NumberFormatException ex) {
+      return ex.toString();
+    }
+    PerceptionProbabilities entry = new PerceptionProbabilities(instance, 
+                                                                hueMeanD, 
+                                                                hueSDD, 
+                                                                volMeanD, 
+                                                                volSDD);
     objects.add(entry);
-    return objects.size();
+    return "number of objects for classification: " + objects.size();
   }
 }
