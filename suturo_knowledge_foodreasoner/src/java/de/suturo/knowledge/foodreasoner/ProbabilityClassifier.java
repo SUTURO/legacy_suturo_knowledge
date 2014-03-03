84b596d8c2671c0adb7087a2734003b68753ad14
ofenrohr <ofenrohr@tzi.de>
2014-03-03 15:24:46 +0100

    KR-64 #time 3h weka basics

--- suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/ProbabilityClassifier.java
+++ suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/ProbabilityClassifier.java
@@ -1,5 +1,9 @@
 package de.suturo.knowledge.foodreasoner;
 
+import ros.pkg.geometry_msgs.msg.Point;
+import ros.pkg.geometry_msgs.msg.Pose;
+import ros.pkg.suturo_perception_msgs.msg.PerceivedObject;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
--- suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/WekaClassifier.java
+++ suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/WekaClassifier.java
@@ -1,10 +1,53 @@
 package de.suturo.knowledge.foodreasoner;
 
 import ros.pkg.geometry_msgs.msg.Point;
+import ros.pkg.geometry_msgs.msg.Pose;
 import ros.pkg.suturo_perception_msgs.msg.PerceivedObject;
 
+import weka.core.Instances;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 class WekaClassifier implements ObjectClassifier {
-  public String classifyPerceivedObject(PerceivedObject percObject) {
+  public WekaClassifier() {
+    String foodreasoner_path = executeCommand("rospack find suturo_knowledge_foodreasoner");
+    String arff_path = foodreasoner_path + "/raw_data/milestone4.arff";
+    
+    System.out.println("arff path: "+arff_path);
+
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(arff_path));
+      Instances data = new Instances(reader);
+        reader.close();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+
+  }
+
+  public String classifyPerceivedObject(PerceivedObject po) {
     return "";
+  }
+
+  /**
+   * executes a command. taken from http://www.mkyong.com/java/how-to-execute-shell-command-from-java/
+   */
+  private String executeCommand(String command) {
+    StringBuffer output = new StringBuffer();
+    Process p;
+    try {
+      p = Runtime.getRuntime().exec(command);
+      p.waitFor();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+      String line = "";     
+      while ((line = reader.readLine())!= null) {
+        output.append(line + "\n");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return output.toString();
   }
 }
