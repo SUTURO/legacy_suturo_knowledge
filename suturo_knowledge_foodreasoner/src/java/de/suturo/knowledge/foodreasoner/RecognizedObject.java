6be88865dbd62c115e232d721d3b048569f69767
Moritz Horstmann <mhorst@tzi.de>
2014-04-15 15:38:13 +0200

    Extreme refactoring.

---
+++ suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/RecognizedObject.java
@@ -0,0 +1,46 @@
+package de.suturo.knowledge.foodreasoner;
+
+import ros.pkg.suturo_perception_msgs.msg.PerceivedObject;
+
+/**
+ * Representation of a recognized object
+ * 
+ * @author Moritz
+ * 
+ */
+public class RecognizedObject extends AbstractObject {
+
+	private final String identifier;
+
+	/**
+	 * Create a new instance from a perception
+	 * 
+	 * @param po
+	 *            PerceivedObject
+	 * @param identifier
+	 *            Identifier
+	 */
+	public RecognizedObject(PerceivedObject po, String identifier) {
+		super(po);
+		this.identifier = identifier;
+	}
+
+	/**
+	 * Create a new instance from a previously perceived object
+	 * 
+	 * @param object
+	 *            AbstractObject instance
+	 * @param identifier
+	 *            Identifier
+	 */
+	public RecognizedObject(AbstractObject object, String identifier) {
+		super(object);
+		this.identifier = identifier;
+	}
+
+	@Override
+	public String getIdentifier() {
+		return identifier;
+	}
+
+}
---
+++ suturo_knowledge_foodreasoner/src/java/de/suturo/knowledge/foodreasoner/exception/TFException.java
@@ -0,0 +1,75 @@
+package de.suturo.knowledge.foodreasoner.exception;
+
+/**
+ * This exception is thrown if a TF conversion fails.
+ * 
+ * @author Moritz
+ * 
+ */
+public class TFException extends Exception {
+
+	private static final long serialVersionUID = -866117225046165234L;
+
+	/**
+	 * Default constructor
+	 */
+	public TFException() {
+		super("An error occured while executing a TF transformation");
+	}
+
+	/**
+	 * Default constructor with cause
+	 * 
+	 * @param cause
+	 */
+	public TFException(Throwable cause) {
+		super("An error occured while executing a TF transformation", cause);
+	}
+
+	/**
+	 * For TF lookup failures
+	 * 
+	 * @param lookupFrame
+	 *            Frame failed to get looked up
+	 */
+	public TFException(String lookupFrame) {
+		super("Could not find " + lookupFrame + " in the TF cache!");
+	}
+
+	/**
+	 * For TF lookup failures with cause
+	 * 
+	 * @param lookupFrame
+	 *            Frame failed to get looked up
+	 * @param cause
+	 */
+	public TFException(String lookupFrame, Throwable cause) {
+		super("Could not find " + lookupFrame + " in the TF cache!", cause);
+	}
+
+	/**
+	 * For TF transform failures
+	 * 
+	 * @param sourceFrame
+	 *            Source frame
+	 * @param targetFrame
+	 *            Target frame
+	 */
+	public TFException(String sourceFrame, String targetFrame) {
+		super("Could not transform from " + sourceFrame + " to " + targetFrame);
+	}
+
+	/**
+	 * For TF transform failures with cause
+	 * 
+	 * @param sourceFrame
+	 *            Source frame
+	 * @param targetFrame
+	 *            Target frame
+	 * @param cause
+	 */
+	public TFException(String sourceFrame, String targetFrame, Throwable cause) {
+		super("Could not transform from " + sourceFrame + " to " + targetFrame,
+				cause);
+	}
+}
